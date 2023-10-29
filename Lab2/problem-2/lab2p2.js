87ec277ab037419c3bf32e9887b48ac3012b0367
Kylreia <rumbauakeith@gmail.com>
2023-10-29 23:57:27 +0000

    added js for problem-2

--- Lab2/problem-2/lab2p2.js
+++ Lab2/problem-2/lab2p2.js
@@ -1,76 +1,16 @@
-// Question 1 from Part 2
+function titleChecker(data) {
+	const titles = data.title.split(" ")
+	return titles.length > 6;
+} // end function titleChecker()
 
-fetch('https://jsonplaceholder.typicode.com/posts').then(response => 
-{
-    return response.json()
+fetch('https://jsonplaceholder.typicode.com/posts').then(response => response.json()).then(json => {
 
+    // 1.
+	const titleList = json.filter(titleChecker);
+	console.log("1.", titleList)
+
+	// 2.
+	const wordFrequency = json.flatMap(post => post.body.split(/\W+/)).reduce((frequencyMap, word) => ({
+		...frequencyMap, [word]: (frequencyMap[word] || 0) + 1}), {});
+	console.log("2.", wordFrequency);
 })
-.then(data => 
-{
-    console.log(data);
-    console.log("The following post titles have more than 6 words: ")
-
-    obj = data.map(posts => 
-    {
-        var newObj = posts.title;
-        
-        return newObj;
-
-    });
-
-    obj.map(element => 
-    {
-        var spaceCount = (element.split(" ").length - 1);
-        if (spaceCount >= 6) 
-        {
-            console.log(element);
-        }  
-    });  
-});
-
-// Question 2 from Part 2
-fetch('https://jsonplaceholder.typicode.com/posts').then(response => response.json())
-.then(data => 
-{
-    obj = data.map(book => 
-    {
-        var newObj = book.body;
-
-        return newObj;
-    });
-
-    wordFreq(obj);
-});
-
-function wordFreq(data)
-{
-    var array = [];
-    for(var i=0; i < data.length; i++)
-    {
-        array.push(data[i]); 
-    }
-
-    var words = new Array();
-    var x = array.toString();
-
-    words = x.split(" ");
-    var unique_word = {};
-
-    for (var i = 0; i < words.length; i++) 
-    {
-        var word = words[i];
-
-        if (word in unique_word)
-        {
-            var count  = unique_word[word];
-            count ++;
-            unique_word[word] = count;
-        }
-        else
-        {
-            unique_word[word] = 1;
-        }
-    }
-    console.log("The word frequency map is: ")
-    console.log(unique_word);
-}
